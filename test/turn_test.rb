7e76e0d8a652719090fe9e52e62da8453dec75d7
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-30 16:36:27 -0700

    Changed test name to test_feedback

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -27,7 +27,7 @@ class TurnTest < Minitest::Test
     assert_equal card, turn.card
   end
 
-  def test_feedback_for_correct?
+  def test_feedback
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
