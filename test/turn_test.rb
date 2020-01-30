2f66789c9e7075612af64edc3678935d8eb3383f
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-30 16:23:33 -0700

    Add card attr_reader test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -6,14 +6,6 @@ require './lib/card'
 
 class TurnTest < Minitest::Test
 
-  attr_reader
-
-  def test_card_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    assert_instance_of Card, card
-  end
-
   def test_turn_exists
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
@@ -21,25 +13,32 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
-  def test_if_guess_is_correct
+  def test_if_it_has_guess
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     assert_equal "Juneau", turn.guess
   end
 
+  def test_it_has_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal card, turn.card
+  end
+
   def test_feedback_for_correct_answer
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
-    assert_equal "Correct!" turn.guess
+    assert_equal "Correct!", turn.feedback
   end
 
   def test_if_guess_is_incorrect
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
 
-    assert_equal "Saturn", turn.guess
+    assert_equal "Incorrect.", turn.feedback # move to correct test
   end
-
+#add test for correct?
 end
