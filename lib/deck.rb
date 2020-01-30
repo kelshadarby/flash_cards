4ec9d5a3341c9940919798c349d3fb06ded243cf
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-30 07:42:34 -0700

    Add if to cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,4 +12,11 @@ class Deck
     @cards.length
   end
 
+  def cards_in_category(category_name)
+    cards.each do |card|
+      if @Card.category == category_name
+      end # if
+    end # each
+  end # cards_in_category
+
 end
