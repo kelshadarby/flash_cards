c0afb61976b5b7f7a303ab68a820c92b3e1a11e1
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-30 07:45:57 -0700

    Add expected value to test_it_has_cards_in_category

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -50,7 +50,7 @@ class DeckTest < Minitest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
-    assert_equal
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
   end
 
 end
