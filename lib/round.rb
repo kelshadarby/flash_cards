c2d6ed46292c70132fb53016b2f635bc65b7d041
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-30 21:51:43 -0700

    Add base for current_card method.

--- lib/round.rb
+++ lib/round.rb
@@ -11,6 +11,21 @@ class Round
     @turns = []
   end
 
+  def current_card
+   if Deck.cards[1] == Turn.card
+   end
+  end
+
 end # class
 
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+deck1 = Deck.new([card_1, card_2, card_3])
+round1 = Round.new(deck1)
+
+
+
+ #if card = card then is current_card
 # use @ for attribute of item being created
