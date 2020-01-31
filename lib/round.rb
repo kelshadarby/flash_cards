18fc04d4b0d58456e2e3e05a2e83a081a6850678
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-31 08:37:21 -0700

    Add current_card method

--- lib/round.rb
+++ lib/round.rb
@@ -12,18 +12,11 @@ class Round
   end
 
   def current_card
-   if Deck.cards[1] == Turn.card
-   end
+   @deck.cards.first
   end
 
 end # class
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-deck1 = Deck.new([card_1, card_2, card_3])
-round1 = Round.new(deck1)
 
 
 
