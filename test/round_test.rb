0caf3cea71c0d8870cd378d6eb9bd019222ac427
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-31 18:40:10 -0700

    Add number_correct_by_category test

--- test/round_test.rb
+++ test/round_test.rb
@@ -122,24 +122,39 @@ class RoundTest < Minitest::Test
     new_turn = round.take_turn("Juneau")
 
     assert_equal 1, round.number_correct
+
+    new_turn2 = round.take_turn("Venus")
+
+    assert_equal 1, round.number_correct
   end
+
+  def test_it_can_return_feedback
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+    new_turn2 = round.take_turn("Venus")
+
+    assert_equal "Incorrect.", round.turns.last.feedback
+
+    new_turn2 = round.take_turn("Venus")
+  end
+
+  def test_it_can_tell_number_correct_by_category
+
+
+    assert_equal 1, round.number_correct_by_category(:Geography)
+  end
+
 
 end #class
 
 
 
-# pry(main)> round.take_turn("Venus")
-# #=> #<Turn:0x00007f972a215b38...>
-#
-# pry(main)> round.turns.count
-# #=> 2
-#
-# pry(main)> round.turns.last.feedback
-# #=> "Incorrect."
-#
-# pry(main)> round.number_correct
-# #=> 1
-#
 # pry(main)> round.number_correct_by_category(:Geography)
 # #=> 1
 #
