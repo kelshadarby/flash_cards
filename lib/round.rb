af77ad37b781b01dd81ab50af944a16ea6f07626
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-01 22:43:46 -0700

    Add start method for one round

--- lib/round.rb
+++ lib/round.rb
@@ -1,4 +1,5 @@
 require './lib/turn'
+require './lib/deck'
 
 class Round
 
@@ -17,15 +18,14 @@ class Round
     turn = Turn.new(guess, current_card)
     @turns << turn
     @deck.cards.rotate!
-    @deck.cards.pop
     turn
   end
 
   def number_correct
       @turns.count do |turn|
         turn.correct?
-      end #loop
-  end #method
+      end
+  end
 
   def number_correct_by_category(category)
     @turns.count do |turn|
@@ -40,7 +40,36 @@ class Round
   end
 
   def percent_correct_by_category(category)
-    (number_correct_by_category(category).to_f/number_correct.to_f)*100
+    (number_correct_by_category(category).to_i/@round.deck.cards_in_category(category).count.to_i)*100
   end
 
+  def start(round)
+    @round = round
+
+    puts "Welcome! You're playing with #{@deck.cards.length} cards."
+    puts "-" * 50
+
+    card_index = @deck.cards.length
+
+    @deck.cards.each do |card|
+      puts "This is card number  out of #{card_index}."
+      puts "Question: #{@round.current_card.question}"
+
+      new_turn = @round.take_turn(gets.chomp)
+
+      puts new_turn.feedback
+    end
+
+    puts "****** Game over! ******"
+    puts "You had #{@round.number_correct} correct guesses out of #{card_index} for a total score of #{@round.percent_correct.round(0)}%."
+
+  @deck.cards.each do |card|
+    puts "#{card.category} - #{@round.percent_correct_by_category(card.category)}% correct."
   end
+
+
+
+
+  end
+
+end
